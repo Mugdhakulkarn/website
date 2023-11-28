bec9bec27dbc26825ed1410e768ec430991efe50
Mugdha Kulkarni <mugdhakulkarni1997@gmail.com>
2023-11-28 19:16:51 +0530

    project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CoursesComponent } from './courses/courses.component';
+import { PlacementComponent } from './placement/placement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    GalleryComponent,
+    CoursesComponent,
+    PlacementComponent
   ],
   imports: [
     BrowserModule,
